fbc01639ac21aa4ba902ffb24d02d90443f4e609
ferigalung <ferigalungcom@gmail.com>
2024-09-25 14:54:23 +0700

    add error handling when param name is empty

--- learning-go-module/greetings/greetings.go
+++ learning-go-module/greetings/greetings.go
@@ -1,9 +1,18 @@
 package greetings
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+)
 
 // Hello returns a greeting for the named person.
-func Hello(name string) string {
+// this func must return string, and error. If succeed, return nil on second return val. If error occurred, return instance of "errors" package on second return val
+func Hello(name string) (string, error) {
+	// return error when name is empty
+	if name == "" {
+		return "", errors.New("empty name")
+	}
+
 	message := fmt.Sprintf("Hi, %v. Welcome!", name)
-	return message
+	return message, nil
 }
